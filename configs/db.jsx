9b45ddc4abd0df65e88c57c0d5b1900622de614a
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-04-10 01:02:16 +0530

    first commit

--- configs/db.jsx
+++ configs/db.jsx
@@ -1,8 +1,6 @@
+
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-// console.log(process.env.DATABASE_URL);
 
-const sql = neon(process.env.NEXT_PUBLIC_DB_CONNECTION_STRING);
-
+const sql = neon(process.env.NEXT_PUBLIC_DB_CONNECTION_STRING); // make sure this is defined
 export const db = drizzle(sql);
-
--- drizzle.config.js
+++ drizzle.config.js
@@ -1,9 +1,8 @@
-/** @type { import("drizzle-kit").Config } */
+// drizzle.config.js (testing only)
 export default {
   schema: "./configs/Schema.jsx",
-  dialect: 'postgresql',
+  dialect: "postgresql",
   dbCredentials: {
-    url: process.env.NEXT_PUBLIC_DB_CONNECTION_STRING,
-    
-  }
+    url: "postgresql://neondb_owner:npg_rRXGZ67TIjES@ep-morning-boat-a5jc9zci-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require",
+  },
 };
